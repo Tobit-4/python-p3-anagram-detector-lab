ab2ac534228ce401ff05f144f6e064fa61e675bf
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-05-11 20:38:17 +0300

    made changes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+class Anagram:
+    pass
+
+    def __init__(self,word):
+        self.word = word
+
+    def match(self,word_list):
+        matches = []
+        sorted_given_word = sorted(self.word)
+
+        for candidate in word_list:
+            normalized_candidate = candidate.lower()
+            if normalized_candidate == sorted_given_word:
+                continue
+            if sorted(normalized_candidate) == sorted_given_word:
+                matches.append(candidate)
+
+        return matches
